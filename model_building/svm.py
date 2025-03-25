f9dcfa591b08a298b7ee38328b9756e55b590e7b
Estefany Valdivieso <estefanyval30@gmail.com>
2025-03-25 03:20:10 -0400

    feat: improve pipelines and enable feature selection

--- model_building/svm.py
+++ model_building/svm.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
@@ -12,7 +13,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data_modeling.cleaning_pipeline import CleaningPipeline
 from data_modeling.transformation_pipeline import TransformationPipeline
-# from data_modeling.feature_selection import perform_feature_selection
+from data_modeling.feature_selection import perform_feature_selection
 
 # Load and clean the dataset
 raw_df = pd.read_csv("KSI_dataset.csv")
@@ -30,25 +31,42 @@ transformer = TransformationPipeline()
 X_train_transformed = transformer.fit_transform(X_train_raw, y_train)
 X_test_transformed = transformer.transform(X_test_raw)
 
-# Feature Selection - This step is too aggressive and reduces the model's performance so it's commented out
-# X_train_selected, selected_features = perform_feature_selection(X_train_transformed, y_train)
-# X_test_selected = X_test_transformed[selected_features]
+# Feature Selection using RFE with RandomForestClassifier
+X_train_selected, selected_features = perform_feature_selection(X_train_transformed, y_train, transformer.get_feature_names())
+X_test_selected = X_test_transformed[selected_features]
 
 # Handle class imbalance in the training set with SMOTE
 print("\nTraining values before SMOTE:\n", y_train.value_counts())
-# X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_selected, y_train)
-X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_transformed, y_train)
+# X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_transformed, y_train)
+X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_selected, y_train)
 print("\nTraining values after SMOTE:\n", y_train_balanced.value_counts())
 
-# Train a Simple SVM
+# Handle class imbalance in the testing set with SMOTE - this is useful to evaluate the model in a balanced scenario
+print("\nTesting values before SMOTE:\n", y_test.value_counts())
+# X_test_balanced, y_test_balanced = SMOTE(random_state=93).fit_resample(X_test_transformed, y_test)
+X_test_balanced, y_test_balanced = SMOTE(random_state=93).fit_resample(X_test_selected, y_test)
+print("\nTesting values after SMOTE:\n", y_test_balanced.value_counts())
+
+# Train a SVM model
 print("\n--- Training the SVM model ---")
 model = SVC(kernel='rbf', C=10, gamma=0.1, class_weight='balanced', random_state=93)
 model.fit(X_train_balanced, y_train_balanced)
 
-# Evaluate the model
-# y_pred = model.predict(X_test_selected)
-y_pred = model.predict(X_test_transformed)
+# Evaluate the model - first with the original testing set, then with the balanced testing set
+y_pred = model.predict(X_test_selected)
 
+print("\nModel evaluation in the original testing set:")
 print("\nAccuracy:", round(accuracy_score(y_test, y_pred), 4))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+y_pred_balanced = model.predict(X_test_balanced)
+
+print("\n\nModel evaluation in the balanced testing set (after SMOTE):")
+print("\nAccuracy:", round(accuracy_score(y_test_balanced, y_pred_balanced), 4))
+print("\nClassification Report:\n", classification_report(y_test_balanced, y_pred_balanced))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test_balanced, y_pred_balanced))
+
+# Save the model as a pkl file
+joblib.dump(model, 'svm_model.pkl')
+print("\nModel saved as 'svm_model.pkl'")
