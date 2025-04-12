e52cc7a6afd8766b676d7957bc111a00752828f3
Estefany Valdivieso <estefanyval30@gmail.com>
2025-04-12 05:53:36 -0400

    feat(svm): apply randomized search CV

--- model_building/svm.py
+++ model_building/svm.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import joblib
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from imblearn.over_sampling import SMOTE
@@ -31,25 +31,51 @@ transformer = TransformationPipeline()
 X_train_transformed = transformer.fit_transform(X_train_raw, y_train)
 X_test_transformed = transformer.transform(X_test_raw)
 
+# Add data_modeling to sys.path
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATA_MODELING_DIR = os.path.abspath(os.path.join(BASE_DIR, '../data_modeling'))
+sys.path.append(DATA_MODELING_DIR)
+
+# Save the transformation pipeline as a pkl file
+joblib.dump(transformer, os.path.join(DATA_MODELING_DIR, 'transformation_pipeline.pkl'))
+print("\nTransformation pipeline saved as 'transformation_pipeline.pkl'")
+
 # Feature Selection using RFE with RandomForestClassifier
 X_train_selected, selected_features = perform_feature_selection(X_train_transformed, y_train, transformer.get_feature_names())
 X_test_selected = X_test_transformed[selected_features]
 
 # Handle class imbalance in the training set with SMOTE
 print("\nTraining values before SMOTE:\n", y_train.value_counts())
-# X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_transformed, y_train)
 X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_selected, y_train)
 print("\nTraining values after SMOTE:\n", y_train_balanced.value_counts())
 
 # Handle class imbalance in the testing set with SMOTE - this is useful to evaluate the model in a balanced scenario
 print("\nTesting values before SMOTE:\n", y_test.value_counts())
-# X_test_balanced, y_test_balanced = SMOTE(random_state=93).fit_resample(X_test_transformed, y_test)
 X_test_balanced, y_test_balanced = SMOTE(random_state=93).fit_resample(X_test_selected, y_test)
 print("\nTesting values after SMOTE:\n", y_test_balanced.value_counts())
 
 # Train a SVM model
-print("\n--- Training the SVM model ---")
-model = SVC(kernel='rbf', C=10, gamma=0.1, class_weight='balanced', random_state=93)
+print("\n--- SVM model ---")
+
+param_distributions = {
+    'C': [0.1, 1, 10, 100],
+    'gamma': [0.01, 0.1, 1, 10],
+    'kernel': ['rbf', 'poly']
+}
+
+random_search = RandomizedSearchCV(
+    estimator=SVC(class_weight='balanced', random_state=93),
+    param_distributions=param_distributions,
+    n_iter=20,
+    cv=3,
+    verbose=2,
+    n_jobs=-1
+)
+
+random_search.fit(X_train_balanced, y_train_balanced)
+print("\nBest Params:", random_search.best_params_) # kernel='rbf', C=100, gamma=1
+
+model = random_search.best_estimator_
 model.fit(X_train_balanced, y_train_balanced)
 
 # Evaluate the model - first with the original testing set, then with the balanced testing set
