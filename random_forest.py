3317d73b263be5bb7fae7f3c91007c77ff474004
Michael-318 <maikoasfeha@gmail.com>
2025-04-12 11:18:17 -0400

    updated commit

--- random_forest.py
+++ random_forest.py
@@ -36,7 +36,7 @@ y = clean_df['ACCLASS']
 X = clean_df.drop(columns='ACCLASS')
 
 # Split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=64)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=93)
 
 # Apply the transformation pipeline
 transformer = TransformationPipeline()
@@ -47,17 +47,22 @@ X_test_transformed = transformer.transform(X_test)
 X_train_selected, selected_features = perform_feature_selection(X_train_transformed, y_train, transformer.get_feature_names())
 X_test_selected = X_test_transformed[selected_features]
 
-# Handle class imbalance in the training set with SMOTE (not on test set)
+# Handle class imbalance in the training set with SMOTE
 print("\nTraining values before SMOTE:\n", y_train.value_counts())
+# X_train_balanced, y_train_balanced = SMOTE(random_state=93).fit_resample(X_train_transformed, y_train)
 X_train_balanced, y_train_balanced = SMOTE(random_state=64).fit_resample(X_train_selected, y_train)
 print("\nTraining values after SMOTE:\n", y_train_balanced.value_counts())
 
-print(selected_features.dtypes)
+# Handle class imbalance in the testing set with SMOTE - this is useful to evaluate the model in a balanced scenario
+print("\nTesting values before SMOTE:\n", y_test.value_counts())
+# X_test_balanced, y_test_balanced = SMOTE(random_state=93).fit_resample(X_test_transformed, y_test)
+X_test_balanced, y_test_balanced = SMOTE(random_state=64).fit_resample(X_test_selected, y_test)
+print("\nTesting values after SMOTE:\n", y_test_balanced.value_counts())
 
 
 ######################
 '''Building the model'''
-rf_model = RandomForestClassifier(random_state=64, class_weight='balanced')
+rf_model = RandomForestClassifier(random_state=93, class_weight='balanced')
 
 # Fit the initial Random Forest model on the balanced training data
 rf_model.fit(X_train_balanced, y_train_balanced)
@@ -70,6 +75,14 @@ print("\nModel evaluation in the original testing set:")
 print("\nAccuracy:", round(accuracy_score(y_test, y_pred), 4))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+y_pred_balanced = rf_model.predict(X_test_balanced)
+
+print("\n\nModel evaluation in the balanced testing set (after SMOTE):")
+print("\nAccuracy:", round(accuracy_score(y_test_balanced, y_pred_balanced), 4))
+print("\nClassification Report:\n", classification_report(y_test_balanced, y_pred_balanced))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test_balanced, y_pred_balanced))
+
 
 ##########################
 '''Fine Tuning'''
@@ -90,7 +103,7 @@ random_search = RandomizedSearchCV(estimator=rf_model,              # Random For
                                    cv=5,                            # Number of folds for cross-validation
                                    n_jobs=-1,                       # Use all available cores
                                    verbose=1,                       # Verbosity level
-                                   random_state=64)                 # Random state for reproducibility
+                                   random_state=93)                 # Random state for reproducibility
 
 # Fit the model using RandomizedSearchCV
 random_search.fit(X_train_balanced, y_train_balanced)
@@ -107,7 +120,7 @@ print("\nClassification Report:\n", classification_report(y_test, y_test_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_test_pred))
 
 # Save the best estimator (fine-tuned model)
-joblib.dump(best_rf_random, 'C:/Users/maiko/Downloads/RF_model.pkl')
+joblib.dump(best_rf_random, 'C:/Users/maiko/Downloads/rf_model.pkl')
 
 
 
